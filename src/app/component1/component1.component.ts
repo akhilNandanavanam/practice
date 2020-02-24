33d2cec6caed27edcfc8945c8989e57722c82007
Akhil Nandanavanam <akhil.nandanavanam@trackercorp.com>
2020-02-24 08:14:05 +0530

    Form Groups

--- src/app/component1/component1.component.ts
+++ src/app/component1/component1.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, Subject } from 'rxjs';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-component1',
   templateUrl: './component1.component.html',
@@ -19,18 +19,28 @@ export class Component1Component implements OnDestroy, OnInit {
   isError: boolean;
   VALIDATION_ERROR_REQUIRED_TXT: string = "Please enter a Value";
   VALIDATION_MIN_LENGTH: string = "Please enter a Correct Value";
+  myFirstFormGroup: FormGroup;
+
 
   // FormControl -> FormGroup -> FormArray
-  constructor(private activatedRoute: ActivatedRoute, 
+  constructor(private activatedRoute: ActivatedRoute,
               private router: Router) { }
 
   ngOnInit() {
+    this.myFirstFormGroup = new FormGroup({
+      firstName: new FormControl('',Validators.compose([
+        Validators.required,
+        Validators.pattern('[a-zA-Z0-9]+')
+      ])),
+      numberForm : new FormControl('', Validators.required),
+      selectBox: new FormControl('', Validators.required)
+    });
     this.options = ['Akhil', 'Pratyusha', 'Scooby Doo'];
     // this.formValue = 'Akhil'
     this.title = 'asdfghj';
-    this.numberForm = new FormControl(null);
-    this.selectBoxForm = new FormControl(null);
-    this.myField1 = new FormControl(null, Validators.required);
+    // this.numberForm = new FormControl(null);
+    // this.selectBoxForm = new FormControl(null);
+    // this.myField1 = new FormControl(null, Validators.required);
     // this.activatedRoute.params.subscribe(res=>{
     //   console.log(res);
     //   this.array1.forEach(element=>{
@@ -53,9 +63,9 @@ export class Component1Component implements OnDestroy, OnInit {
   //   return true;
   // }
   showFormValue(name: string){
-    console.log('Text Field Value---->', this.myField1.value);
-    console.log('Number Field Value---->', this.numberForm.value);
-    console.log('Select Field Value---->', this.selectBoxForm.value);
+   console.log('Form Group Value---->', this.myFirstFormGroup.value);
+   console.log('Is form group Valid?---->', this.myFirstFormGroup.valid);
+   console.log('Is first name valid?----?', this.myFirstFormGroup.get('numberForm').value);
     // if(this.myField1.valid){
     //   this.isError = false;
     //   this.InputError = null;
@@ -64,5 +74,8 @@ export class Component1Component implements OnDestroy, OnInit {
     //   this.isError = true;
     //   this.InputError = this.VALIDATION_ERROR_REQUIRED_TXT;
     // }
+  }
+  captureInput($event){
+    console.log($event.target.value);
   }
 }
